5f8fa3f50af0e8451161458c4547b227a512e01d
N1ghtmarex <andreyshe22@gmail.com>
2026-01-18 20:42:01 +0700

    feat: Добавлен Keycloak

---
+++ Keycloak/Models/KeycloakApiModels/KeycloakClientSecretModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Keycloak.Models.KeycloakApiModels
+{
+    public class KeycloakClientSecretModel
+    {
+        [JsonPropertyName("type")]
+        public required string Type { get; set; }
+
+        [JsonPropertyName("value")]
+        public required string Value { get; set; }
+    }
+}
