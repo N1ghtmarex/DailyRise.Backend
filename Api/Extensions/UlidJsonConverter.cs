17ded23fba51745805a243783cce43bf9d24e07f
N1ghtmarex <andreyshe22@gmail.com>
2026-01-18 21:07:37 +0700

    feat: Настроен Keycloak и Swagger + Добавлен UlidSchemaFilter + Добавлены конфигурации для Keycloak + Добавлен EnumSchemaFilter + Добавлена конфигурация для Swagger * Добавлены необходимые ссылки для Keyloak

---
+++ Api/Extensions/UlidJsonConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Api.Extensions;
+
+[ExcludeFromCodeCoverage]
+public class UlidJsonConverter : JsonConverter<Ulid>
+{
+    public override Ulid Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var s = reader.GetString();
+        return Ulid.Parse(s);
+    }
+
+    public override void Write(Utf8JsonWriter writer, Ulid value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString());
+    }
+}
